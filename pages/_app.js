e7f813b95efbf08a87ba293a8f28618765dbd31b
dreammstudio <eneskamci10@gmail.com>
2021-12-05 17:37:20 +0300

    Website

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
-
+import Head from 'next/head'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+      <title>Kanban</title>
+    </Head>
+  <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp
